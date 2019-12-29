2bf5e19e621f5c7595542decc5baf099e508aa6d
Vidit Singh Gautam <viditsingh329@outlook.com>
2019-12-29 22:20:44 +0530

    javascript revised

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
 var i;
+var a=[];
 for(i=0;i<document.querySelectorAll(".drum").length;i=i+1)
 {
   document.querySelectorAll(".drum")[i].addEventListener("click", gotClicked);
 }
 function gotClicked()
 {
-  
+  var a=this.innerHTML;
+  if(a=="w")
+  {
+
+  }
+  else if
+  var audio=new Audio('sounds/tom-1.mp3');
+  audio.play();
 }
