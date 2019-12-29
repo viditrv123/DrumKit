5cc0e5b3bf18c2f9599204980897dd0771af598c
Vidit Singh Gautam <viditsingh329@outlook.com>
2019-12-29 13:53:35 +0530

    INITIAL COMMIT

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var i;
+for(i=0;i<document.querySelectorAll(".drum").length;i=i+1)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click", gotClicked);
+}
+function gotClicked()
+{
+  
+}
